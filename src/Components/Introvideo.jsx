65b50af5fd18d9c3d292063d56d57a8310c7e70c
shrikantlspl <shrikant.lspl@gmail.com>
2024-02-06 10:07:12 +0530

    changed Homepage url

--- src/Components/Introvideo.jsx
+++ src/Components/Introvideo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import video from "../Assets/Steel Shots Making Process Rotocast.mp4";
+import video from "../Assets/Steel Shots Making Process.mp4";
 
 const Introvideo = () => {
   return (
